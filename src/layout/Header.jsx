2dd3e68a9863a295044d151bfe9128d351fb988e
Dmitry A <adcntt@gmail.com>
2022-09-06 22:17:52 +0300

    fetching data

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
       <nav className="deep-purple lighten-1">
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">
-            Movies
+            Avant-Garde
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,22 @@
-function Main() {
-  return <main className="container content">Avant-garde Movies Application</main>;
+import React from 'react';
+import { Movies } from '../components/Movies';
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=5f5a59ab&s=matrix')
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return <main className="container content">{movies.length ? <Movies movies={this.state.movies} /> : <h3>Loading...</h3>}</main>;
+  }
 }
 
 export { Main };
