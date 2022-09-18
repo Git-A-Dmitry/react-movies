324a047aa18599df9782c3332453ce740de395c6
Dmitry A <adcntt@gmail.com>
2022-09-19 02:36:17 +0300

    card-image hover

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,12 +4,13 @@ function Movie(props) {
   const { Title: title, Year: year, imdbID: id, Type: type, Poster: poster } = props;
 
   return (
-    <div id={id} className='card movie card medium'>
+    <div id={id} className='card movie card'>
       <div className='card-image waves-effect waves-block waves-light'>
         <img className='activator' src={poster} alt='movies' />
+        <span className='card-title activator'>{title}</span>
       </div>
       <div className='card-content'>
-        <span className='card-title activator'>{title}</span>
+        {/* <span className='card-title activator'>{title}</span> */}
         <p>
           {year} <span className='right'>{type}</span>
         </p>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,17 +1,17 @@
 function Header() {
   return (
     <div>
-      <nav className="deep-purple lighten-1">
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
-            movie data
+      <nav className='deep-purple lighten-1'>
+        <div className='nav-wrapper'>
+          <a href='#' className='brand-logo'>
+            Movie Fans
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <ul id='nav-mobile' className='right hide-on-med-and-down'>
             <li>
-              <a href="badges.html">Home</a>
+              <a href='badges.html'>Home</a>
             </li>
             <li>
-              <a href="collapsible.html">JavaScript</a>
+              <a href='collapsible.html'>JavaScript</a>
             </li>
           </ul>
         </div>
